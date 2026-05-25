38d7115fecf4d587d8dd800dc5921e071abf1dc9
ScarCertainly <alifreinhartabdullah@gmail.com>
2026-05-25 08:19:26 +0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+    root: path.resolve(__dirname),
+    base: '/',
+    plugins: [
+        react(),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx',
+            ],
+            refresh: true,
+        }),
+    ],
+});
